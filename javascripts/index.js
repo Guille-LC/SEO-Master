f1179a9dcf22ab12f180657e35fdfdfa94667540
guillelavi <guillermo96lc@hotmail.com>
2025-01-30 17:59:30 -0300

    botones de prueba v2

--- javascripts/index.js
+++ javascripts/index.js
@@ -5,3 +5,8 @@ botonUno.addEventListener("click", () => alert("Click uno escuchado!"));
 //Opcion 2 para escuchar eventos click
 let botonDos = document.getElementById("botondos");
 botonDos.onclick = () => alert("Click dos escuchado!")
+
+//Opcion 3 pero con una funcion declarada afuera
+const escucharClick = () => alert("Click tres escuchado!");
+let botonTres = document.getElementById("botontres");
+botonTres.addEventListener("click", escucharClick);
